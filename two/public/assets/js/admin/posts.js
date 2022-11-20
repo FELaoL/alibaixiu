41bc3f45351c4d0a33f6f59b84141a1eb82544f8
FELaoL <felaol@sina.com>
2022-11-20 17:56:20 +0800

    文章列表数据展示

---
+++ two/public/assets/js/admin/posts.js
@@ -0,0 +1,80 @@
+// 文章列表数据展示start
+// 向服务器端发送请求，获取文章列表数据
+$.ajax({
+	url: "/posts",
+	type: "get",
+	success: function (response) {
+		var html = template("postsTpl", response);
+		$("#postsBox").html(html);
+		var page = template("pageTpl", response);
+		$("#page").html(page);
+	},
+	error: function () {
+		alert("获取文章列表失败");
+	}
+});
+
+// 处理日期时间格式
+function formateDate(date) {
+	// 将日期时间字符串转换成日期对象
+	date = new Date(date);
+	return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+}
+// 分页
+function changePage(page) {
+	// 向服务器端发送请求，获取文章列表数据
+	$.ajax({
+		url: "/posts",
+		type: "get",
+		data: {
+			page
+		},
+		success: function (response) {
+			var html = template("postsTpl", response);
+			$("#postsBox").html(html);
+			var page = template("pageTpl", response);
+			$("#page").html(page);
+		},
+		error: function () {
+			alert("获取文章列表失败");
+		}
+	});
+}
+
+// 向服务器端发送请求，索要分类数据
+$.ajax({
+	type: "GET",
+	url: "/categories",
+	success: function (response) {
+		var html = template("categoryTpl", {
+			data: response
+		});
+		$("#categoryBox").html(html);
+	},
+	error: function () {
+		alert("获取分类数据失败");
+	}
+});
+
+// 当用户进行文章列表筛选的时候
+$("#filterForm").on("submit", function () {
+	// 获取到管理员选择的过滤条件
+	const formData = $(this).serialize();
+	// 向服务器端发送请求，获取文章列表数据
+	$.ajax({
+		url: "/posts",
+		type: "get",
+		data: formData,
+		success: function (response) {
+			var html = template("postsTpl", response);
+			$("#postsBox").html(html);
+			var page = template("pageTpl", response);
+			$("#page").html(page);
+		},
+		error: function () {
+			alert("获取文章列表失败");
+		}
+	});
+	// 阻止表单默认提交行为
+	return false;
+});
