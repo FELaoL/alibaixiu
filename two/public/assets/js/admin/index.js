4b79a660ce9296ca2ceb05daeb31f15d16b3ef3f
FELaoL <felaol@sina.com>
2022-11-20 21:22:16 +0800

    仪表盘功能实现

--- two/public/assets/js/admin/index.js
+++ two/public/assets/js/admin/index.js
@@ -1,1 +1,41 @@
+// 查询文章数量 start
+// 获取文章的数量
+$.ajax({
+	type: "GET",
+	url: "/posts/count",
+	success: function (response) {
+		$("#post").html("<strong>" + response.postCount + "</strong>篇文章（<strong>" + response.draftCount + "</strong>篇草稿）");
+	},
+	error: function () {
+		alert("查询文章数量失败");
+	}
+});
+// 查询文章数量end
 
+// 查询分类数量 start
+// 获取分类的数量
+$.ajax({
+	type: "GET",
+	url: "/categories/count",
+	success: function (response) {
+		$("#category").html("<strong>" + response.categoryCount + "</strong>个分类");
+	},
+	error: function () {
+		alert("查询分类数量失败");
+	}
+});
+// 查询分类数量end
+
+// 查询评论数量 start
+// 获取评论的数量
+$.ajax({
+	url: "/comments/count",
+	type: "GET",
+	success: function (response) {
+		$("#comment").html("<strong>" + response.commentCount + "</strong>条评论（<strong>" + response.toCommentCount + "</strong>条待审核）");
+	},
+	error: function () {
+		alert("查询评论数量");
+	}
+});
+// 查询评论数量end
--- two/routes/actions/comment/count.js
+++ two/routes/actions/comment/count.js
@@ -1,8 +1,13 @@
-const { Comment } = require('../../../model/Comment');
+const { Comment } = require("../../../model/Comment");
 
 module.exports = async (req, res) => {
 	// 查询所有文章数量
 	const commentCount = await Comment.countDocuments();
+
+	// 查询所有文章数量
+	const toCommentCount = await Comment.countDocuments({
+		state: 0
+	});
 	// 响应
-	res.send({commentCount});
-}
+	res.send({ commentCount, toCommentCount });
+};
