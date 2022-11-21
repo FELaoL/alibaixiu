b6f88eb2130c3da332f9317545ad6fd54740ed07
FELaoL <felaol@sina.com>
2022-11-21 21:26:03 +0800

    最新评论功能实现

--- two/public/assets/js/common/homeCommon.js
+++ two/public/assets/js/common/homeCommon.js
@@ -1,3 +1,9 @@
+// 处理日期时间格式
+function formateDate(date) {
+	// 将日期时间字符串转换成日期对象
+	date = new Date(date);
+	return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+}
 // 热门推荐start
 // 向服务器端发送请求，索要热门推荐数据
 $.ajax({
@@ -22,6 +28,7 @@ $.ajax({
 });
 // 热门推荐end
 // 随机推荐start
+// 向服务器端发送请求，索要随机推荐数据
 $.ajax({
 	url: "/posts/random",
 	type: "GET",
@@ -47,3 +54,38 @@ $.ajax({
 	}
 });
 // 随机推荐end
+
+// 最新评论start
+// 向服务器端发送请求，索要最新评论数据
+$.ajax({
+	url: "/comments/lasted",
+	type: "get",
+	success: function (response) {
+		var commentTpl = `
+            {{each data}}
+            <li>
+            <a href="javascript:;">
+                <div class="avatar">
+                <img src="{{$value.author.avatar}}" alt="">
+                </div>
+                <div class="txt">
+                <p>
+                    <span>{{$value.author.nickName}}</span>{{$imports.formateDate($value.createAt)}}说:
+                </p>
+                <p>{{$value.content}}</p>
+                </div>
+            </a>
+            </li>  
+            
+            {{/each}}
+        `;
+		var html = template.render(commentTpl, {
+			data: response
+		});
+		$("#commentBox").html(html);
+	},
+	error: function () {
+		alert("获取最新评论失败");
+	}
+});
+// 最新评论end
